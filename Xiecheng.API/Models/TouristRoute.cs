7cd67a27c7e5d504e6e070a3d0e473aeb9d2b9c4
Zirui <ziruiwang1997@gmail.com>
2022-02-26 12:45:35 -0800

    create tourstRoute and touristRoutePicture modes

---
+++ Xiecheng.API/Models/TouristRoute.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Xiecheng.API.Models
+{
+    public class TouristRoute
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(1500)]
+        public string Description { get; set; }
+
+        [Column(TypeName ="decimal(18, 2")]
+        public decimal OriginalPrice { get; set; }
+
+        [Range(0.0, 1.0)]
+        public double? DiscountPresent { get; set; }//？是可空变量
+
+        public DateTime CreateTime { get; set; }
+
+        public DateTime? UpdateTime { get; set; }
+
+        public DateTime? DepartureTime { get; set; }
+
+        [MaxLength]
+        public string Features { get; set; }
+
+        [MaxLength]
+        public string Fees { get; set; }
+
+        [MaxLength]
+        public string Notes { get; set; }
+
+        //以上是基础属性 我们还需要一些外键关联外部models 比如TouristRoutePictures
+
+        public ICollection<TouristRoutePicture> TouristRoutePictures { get; set; }
+            = new List<TouristRoutePicture>();//一对多 可以有多个照片模型 初始化一个数组 更加健壮
+
+    }
+}
---
+++ Xiecheng.API/Models/TouristRoutePicture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Xiecheng.API.Models
+{
+    public class TouristRoutePicture
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }//picture自己的id 主键
+
+        [MaxLength(100)]
+        public string Url { get; set; }//picture url
+
+        //用外键连接一个外部model的时候 我们用这个model的ID作为外键
+        //同时也要绑定这个model本身 即TouristRoute
+        //一个pic最多对应一个旅游路线 所以在这里用路线id作为外键 这个变量命名可以和route里的id不一样
+        //TouristRoute的ID在外部作为外键的时候 自动映射成为TouristRouteId
+        [ForeignKey("TouristRouteId")]
+        public Guid TouristRouteId { get; set; }
+
+        public TouristRoute TouristRoute { get; set; }
+    }
+}
