83473bb5d3ac6d04ce360fdf74e19c77219ccdf1
Zirui <ziruiwang1997@gmail.com>
2022-02-26 18:20:04 -0800

    popping new data

--- Xiecheng.API/Database/AppDbContext.cs
+++ Xiecheng.API/Database/AppDbContext.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Xiecheng.API.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
 
 namespace Xiecheng.API.Database
 {
@@ -15,5 +19,26 @@ namespace Xiecheng.API.Database
         public DbSet<TouristRoutePicture> TouristRoutesPicture { get; set; }
         //这个类注册了context 连接真正的数据库
 
+        //添加种子数据
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<TouristRoute>().HasData(new TouristRoute()
+            //{
+            //    Id = Guid.NewGuid(),
+            //    Title = "ceshititle",
+            //    Description = "shuoming",
+            //    OriginalPrice = 0,
+            //    CreateTime = DateTime.UtcNow
+            //});
+            var touristRouteJsonData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"/Database/touristRoutesMockData.json");
+            IList<TouristRoute> touristRoutes = JsonConvert.DeserializeObject<IList<TouristRoute>>(touristRouteJsonData);
+            modelBuilder.Entity<TouristRoute>().HasData(touristRoutes);//把种子数据保存成json格式 然后一次性写入
+
+            var touristRoutePictureJsonData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"/Database/touristRoutePicturesMockData.json");
+            IList<TouristRoutePicture> touristRoutePictures = JsonConvert.DeserializeObject<IList<TouristRoutePicture>>(touristRoutePictureJsonData);
+            modelBuilder.Entity<TouristRoutePicture>().HasData(touristRoutePictures);
+
+            base.OnModelCreating(modelBuilder);//modelbuilder设置好后写入数据库中
+        }
     }
 }
